bed2ebb0fe9fb9552e17d001edefde5c30d7dce7
costineesti <chiticcostin@yahoo.com>
2025-02-18 20:39:54 +0200

    Quartz sync: Feb 18, 2025, 8:39 PM

--- quartz/components/RecentNotes.tsx
+++ quartz/components/RecentNotes.tsx
@@ -55,7 +55,7 @@ export default ((userOpts?: Partial<Options>) => {
                   </div>
                   {page.dates && (
                     <p class="meta">
-                      <Date date={getDate(cfg, page)!} locale={cfg.locale} />
+                      <Date date={page.dates?.created ?? getDate(cfg, page)!} locale={cfg.locale} />
                     </p>
                   )}
                   {opts.showTags && (
